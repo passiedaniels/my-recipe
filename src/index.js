5cf5b45f7ab24ea88cf53016483723893dcef638
passiedaniels <166223164+passiedaniels@users.noreply.github.com>
2025-06-02 01:32:47 +0300

    added basic form handler and ids

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
+function generate(event) {
+  event.preventDefault();
 
+  new Typewriter("#instruction", {
+    strings: [
+      "Recipe for Beef stew:Ingredient:Beef stew meat,Vegetables: Tender potatoes, onions, carrots and peas, Flour:All-purpose flour,",
+    ],
+    autoStart: true,
+    delay: 100,
+    cursor: "",
+  });
+}
+
+let formElement = document.querySelector("#form-ingredient");
+formElement.addEventListener("submit", generate);
