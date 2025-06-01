f1df57576e0bbc2b3059f120d760f7e6385c8dcf
passiedaniels <166223164+passiedaniels@users.noreply.github.com>
2025-06-02 02:29:50 +0300

    added class

--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
-function generate(event) {
-  event.preventDefault();
-
+function displayAnswer(response) {
+  console.log("generating your recipe");
   new Typewriter("#instruction", {
-    strings: [
-      "Recipe for Beef stew:Ingredient:Beef stew meat,Vegetables: Tender potatoes, onions, carrots and peas, Flour:All-purpose flour,",
-    ],
+    strings: response.data.answer,
     autoStart: true,
-    delay: 100,
+    delay: 1,
     cursor: "",
   });
+}
+
+function generate(event) {
+  let searchInput = document.querySelector("#search-button");
+  event.preventDefault();
+  let prompt = `generate a recipe on ${searchInput.value}`;
+  let context =
+    "you are an inteligent experience good chef with lots of knowledge on food and ingeredient and how they are cooked, please generate a recipe that is short and simple  that will satisfy the user, make sure to follow the search input requests, please put them into different lines using <br> element starting from the heading to the last, remove the stars,use <br> element to separate them into lines starting from ingredients";
+  let apiKey = "943a3ddbo20b374aff624c0t29d891a1";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  axios.get(apiUrl).then(displayAnswer);
+
+  console.log(`generating ${searchInput.value}`);
+  console.log(`prompt:${prompt}`);
+  console.log(`context ${context}`);
 }
 
 let formElement = document.querySelector("#form-ingredient");
